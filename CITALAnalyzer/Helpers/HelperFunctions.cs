cd2d66bbec71e1bc8fe7e97e030f9b5926c25ebc
Till Leiteritz <t.Leiteritz@countit.at>
2026-04-10 12:40:30 +0200

    feat: Rule0007 fixed Regex
    The Regex did not include Numbers which Broke the example
    Optimized The Regex

--- CITALAnalyzer/Helpers/HelperFunctions.cs
+++ CITALAnalyzer/Helpers/HelperFunctions.cs
@@ -11,12 +11,11 @@ public class HelperFunctions
 
     public static bool ContainsSpecialCharacters(string str) =>
         Regex.IsMatch(str, "[^a-zA-Z0-9_]", RegexOptions.Compiled); // allows letters, digits, underscore
-
+    
     public static string RemoveAlpanumericCharacters(string name)
     {
         return Regex.Replace(name, "[^a-zA-Z0-9]", "");
     }
-
 
     // removes all leading uppercase letters except the last one -> First Letter of Object name
     public static string RemovePrefix(string name)
@@ -24,14 +23,12 @@ public class HelperFunctions
         if (string.IsNullOrEmpty(name))
             return name;
 
-        // match ≥3 uppercase letters at start
-        var match = Regex.Match(name, @"^([A-Z]{3,})(.*)$");
+        //^[A-Z0-9]{3,} = from the start match ≥3 uppercase letters or Numbers at start
+        // ([A-Z].+) = match 1 uppercase Letter and everything else
+        var match = Regex.Match(name, @"^[A-Z0-9]{3,}([A-Z].+)", RegexOptions.Compiled);
         if (match.Success)
         {
-            string prefix = match.Groups[1].Value;
-            string rest = match.Groups[2].Value;
-
-            return prefix[^1] + rest;
+            return match.Groups[1].Value;
         }
 
         return name;
